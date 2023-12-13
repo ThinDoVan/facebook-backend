9209dc5ec5f4ec1d93c986d53a97ea6260898243
ThinDoVan <thin.dv.213@gmail.com>
2023-12-13 16:54:49 +0700

    init

---
+++ src/main/java/com/example/facebookbackend/FacebookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.facebookbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacebookBackendApplication.class, args);
+    }
+
+}
