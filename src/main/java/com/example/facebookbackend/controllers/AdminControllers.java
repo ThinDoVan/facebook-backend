475446967528d0477bc3e742ff3a9bb4f6c4c97a
ThinDoVan <thin.dv.213@gmail.com>
2024-01-03 13:29:51 +0700

    Update AdminControllers.java

--- src/main/java/com/example/facebookbackend/controllers/AdminControllers.java
+++ src/main/java/com/example/facebookbackend/controllers/AdminControllers.java
@@ -1,9 +1,18 @@
 package com.example.facebookbackend.controllers;
 
+import com.example.facebookbackend.dtos.response.ReportReqDto;
 import com.example.facebookbackend.entities.User;
 import com.example.facebookbackend.securities.services.UserDetailsImpl;
 import com.example.facebookbackend.services.AdminServices;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping(path = "/admin")
+@Tag(name = "admin")
 public class AdminControllers {
     @Autowired
     AdminServices adminServices;
@@ -21,12 +31,14 @@ public class AdminControllers {
             @RequestParam(required = false) Integer postId,
             @RequestParam(required = false, defaultValue = "0") Integer page,
             @RequestParam(required = false, defaultValue = "5") Integer size) {
-        return ResponseEntity.ok(adminServices.getListPostReport(postId, page, size));
+        Page<ReportReqDto> result = adminServices.getListPostReport(postId, page, size);
+        return ResponseEntity.ok(result);
     }
 
     @GetMapping(path = "/GetReport")
     public ResponseEntity<?> getListReport(@RequestParam Integer reportId) {
-        return ResponseEntity.ok(adminServices.getReportRequest(reportId));
+        ReportReqDto result = adminServices.getReportRequest(reportId);
+        return ResponseEntity.ok(result);
     }
 
     @PutMapping(path = "/ReportHandling")
